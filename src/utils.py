b5704045cd8cd83eeafd34a337115a5721dd0f67
julie <77276652+wonderpompote@users.noreply.github.com>
2022-05-27 18:22:21 +0100

    add comments + small modifications

--- src/utils.py
+++ src/utils.py
@@ -1,26 +1,30 @@
-
-from ctypes import WinError
-from time import sleep
+from datetime import datetime
 
 separator = "<sep>".encode()
-separator_id = "<id_sep>"
+id_separator = "<id_sep>"
+dest_separator = "<dest_sep>".encode()
+message_separator = "<message_sep>".encode()
 hash_algo = "SHA-512"
 number_of_clients = 3
-timeout_socket = 30
+nb_bytes_random = 16
+nb_bytes_session_key = 64
 
-nb_bytes_random = 3
-
+# function to generate a certificate based on the entity's id and public key
 def generate_certificate(id, public_key):
     return {
         "id": id,
-        "timestamp": None, # !!!!!!!!!!!!!!!
+        "timestamp": datetime.now(), # date and time the certificate was generated
         "public_key": public_key
     }
 
+# function to check is socket still open and has not been closed by server
 def is_socket_still_open(socket):
     try:
+        # try sending message to server
         socket.send("test".encode())
         socket.send("test".encode())
+        # if successful --> socket still open
         return True
     except Exception as e:
+        # if something goes wrong --> socket closed
         return False
